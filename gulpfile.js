1beaaf3638a37ab7529e1e76d2e226647ff0564e
Davyd Tevzadze <pawtod20@gmail.com>
2021-03-29 22:36:16 +0300

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -47,8 +47,8 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery-3.5.1.min.js',
 		'app/js/wow.js',
-		'app/js/common.js',
-		'app/js/jquery.malihu.PageScroll2id.min.js' // Always at the end
+		'app/js/jquery.malihu.PageScroll2id.js',
+		'app/js/common.js' // Always at the end
 		], { allowEmpty: true })
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
